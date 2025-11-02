754928f7ad3656d5f936668b3959550957072c4d
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-02 12:16:31 -0600

    Delete tabs.js

--- tabs.js
+++
@@ -1,6 +0,0 @@
-export function switchTab(tabId) {
-  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-  document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
-  document.getElementById(`tab-${tabId}`).classList.add('active');
-  document.getElementById(`content-${tabId}`).classList.add('active');
-}
