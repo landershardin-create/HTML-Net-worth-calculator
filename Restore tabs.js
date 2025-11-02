3f2ba39b1a496bfbd01d7db520042b98ccd08303
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-02 12:11:00 -0600

    Update Restore tabs.js

--- Restore tabs.js
+++ Restore tabs.js
@@ -1,1 +1,6 @@
-
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = JSON.parse(localStorage.getItem("dashboardTabs") || "[]");
+  tabs.forEach(({ tabId, type }) => {
+    createTab(type, tabId); // Modify createTab to accept tabId
+  });
+});
