7fb4ff41b3d99a08b3cbc2521beb2797f91f1121
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-02 12:15:12 -0600

    Delete local persistent storage.js

--- local persistent storage.js
+++
@@ -1,5 +0,0 @@
-function saveTabState(tabId, type) {
-  const tabs = JSON.parse(localStorage.getItem("dashboardTabs") || "[]");
-  tabs.push({ tabId, type });
-  localStorage.setItem("dashboardTabs", JSON.stringify(tabs));
-}
