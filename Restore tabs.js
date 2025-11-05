a96bf12ef9fe36e51170fb404e8780b55a06b15d
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-05 08:51:32 -0600

    Delete Restore tabs.js
    Signed-off-by: Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>

--- Restore tabs.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tabs = JSON.parse(localStorage.getItem("dashboardTabs") || "[]");
-  tabs.forEach(({ tabId, type }) => {
-    createTab(type, tabId); // Modify createTab to accept tabId
-  });
-});
