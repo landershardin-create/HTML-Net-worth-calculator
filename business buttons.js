9f95f0a8b65285f35b780fa39868c8443c18dcd5
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-05 08:51:43 -0600

    Delete business buttons.js
    Signed-off-by: Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>

--- business buttons.js
+++
@@ -1,6 +0,0 @@
-const businessButtons = document.querySelectorAll('#businessGroup button');
-businessButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    console.log(`${btn.id} clicked`);
-  });
-});
