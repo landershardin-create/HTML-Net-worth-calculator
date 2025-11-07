64f28e8d90d7f3a8c81c7029381ee162b3ecc005
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-07 10:36:13 -0600

    Update indexDashboard.js

--- indexDashboard.js
+++ indexDashboard.js
@@ -1,5 +1,32 @@
 import { getUserSession } from '../session/sessionContext.js';
 import { getIndexView } from './indexRoleViews.js';
+import { renderSummaryCards } from './indexSummaryCards.js';
+import { renderFilters } from './indexFilters.js';
+import { showOnboarding } from './indexOnboarding.js';
+import { loadUserPrefs, saveUserPrefs } from './indexStorage.js';
 
-const session = getUserSession();
-const viewComponents = getIndexView(session.role);
+export function renderIndexDashboard(data) {
+  const session = getUserSession();
+  const view = getIndexView(session.role);
+  const prefs = loadUserPrefs(session.userId);
+
+  let html = '';
+
+  if (!prefs.dismissedOnboarding) {
+    html += showOnboarding();
+  }
+
+  if (view.includes('summaryCards')) {
+    html += renderSummaryCards(data);
+  }
+
+  if (view.includes('filters')) {
+    html += renderFilters(data.categories);
+  }
+
+  if (view.includes('fullIndex')) {
+    html += `<div class="index-table">${renderIndexTable(data.entries)}</div>`;
+  }
+
+  return html;
+}
