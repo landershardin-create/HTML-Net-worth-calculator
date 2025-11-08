466d4cbdcadfbc81e079161bda1d96b876b8bd3f
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-08 14:57:41 -0600

    Delete indexOnboarding.js
    Signed-off-by: Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>

--- indexOnboarding.js
+++
@@ -1,9 +0,0 @@
-export function showOnboarding() {
-  return `
-    <div class="onboarding-overlay">
-      <h2>Welcome to your Index Dashboard</h2>
-      <p>This view summarizes your financial position. Use filters to customize your insights.</p>
-      <button onclick="dismissOnboarding()">Got it!</button>
-    </div>
-  `;
-}
